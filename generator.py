3bfde89efd73070f9e225dfb988e283d5d8073f6
Yahyarizwan85 <155488461+Yahyarizwan85@users.noreply.github.com>
2024-01-18 20:52:42 +0500

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,6 @@
+import random 
+roop = "abcefghijklmnopqrstwxyz0123456789ABCDEFGHIJKLMNOPQRSTWXYZ@&*$%#" 
+lenght=int(input("enter your password lenght: "))
+pasword="" 
+for a in range(lenght): pasword+=random.choice(roop) 
+print(pasword)
